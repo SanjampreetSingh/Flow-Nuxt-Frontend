1f67267431768fafdbbab9b7938e32036df47927
Karamveer Singh <kmvsingh007@gmail.com>
2019-09-29 16:15:55 +0530

    navbar check

--- components/layout/default/NavBar.vue
+++ components/layout/default/NavBar.vue
@@ -1,31 +1,29 @@
 <template>
-  <client-only>
-    <header class="columns">
-      <div id="fix-top" class="column navbarScroll">
-        <b-navbar class="container">
-          <template slot="brand">
-            <b-navbar-item tag="router-link" :to="{ path: '/' }">
-              <logo id="logo" />
-            </b-navbar-item>
-          </template>
-          <template slot="end">
-            <b-navbar-item href="#">Demo</b-navbar-item>
-            <b-navbar-item href="#">Documentation</b-navbar-item>
-            <b-navbar-item tag="div">
-              <nuxt-link
-                id="login-btn"
-                to="/login"
-                exact-active-class="is-static"
-                class="button"
-              >
-                Get Started
-              </nuxt-link>
-            </b-navbar-item>
-          </template>
-        </b-navbar>
-      </div>
-    </header>
-  </client-only>
+  <header class="columns">
+    <div id="fix-top" class="column navbarScroll">
+      <b-navbar class="container">
+        <template slot="brand">
+          <b-navbar-item tag="router-link" :to="{ path: '/' }">
+            <logo id="logo" />
+          </b-navbar-item>
+        </template>
+        <template slot="end">
+          <b-navbar-item href="#">Demo</b-navbar-item>
+          <b-navbar-item href="#">Documentation</b-navbar-item>
+          <b-navbar-item tag="div">
+            <nuxt-link
+              id="login-btn"
+              to="/login"
+              exact-active-class="is-static"
+              class="button"
+            >
+              Get Started
+            </nuxt-link>
+          </b-navbar-item>
+        </template>
+      </b-navbar>
+    </div>
+  </header>
 </template>
 <script>
 import Logo from '~/assets/img/logo.svg'
