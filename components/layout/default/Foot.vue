de08fa2fb7d65eb1b1dd2cf7e8c46f8c73604ed4
Karamveer Singh <kmvsingh007@gmail.com>
2019-10-04 20:32:30 +0530

    footer space removed

--- components/layout/default/Foot.vue
+++ components/layout/default/Foot.vue
@@ -1,73 +1,75 @@
 <template>
-  <footer id="footer" class="section has-background-light">
-    <div class="container">
-      <div class="columns">
-        <div class="column has-text-centered">
-          <logo id="foot_logo" />
-        </div>
-        <div class="column has-text-centered">
-          <h3 class="title is-4">Find Us At</h3>
-          <ul id="social">
-            <li>
-              <a
-                href="https://www.twitter.com/Flow_ArtInt"
-                aria-label="twitter"
-                target="_blank"
-                rel="noopener"
-              >
-                <i class="fab fa-twitter"></i>
-                Twitter
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.linkedin.com/company/flow-api"
-                aria-label="linkedin"
-                target="_blank"
-                rel="noopener"
-              >
-                <i class="fab fa-linkedin"></i>
-                LinkedIn
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.instagram.com/flow_artint/"
-                aria-label="instagram"
-                target="_blank"
-                rel="noopener"
-              >
-                <i class="fab fa-instagram"></i>
-                Instagram
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.facebook.com/flowml"
-                aria-label="facebook"
-                target="_blank"
-                rel="noopener"
-              >
-                <i class="fab fa-facebook"></i>
-                Facebook
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div class="column has-text-centered">
-          <h3 class="title is-4">Contact Info</h3>
-          <a
-            href="mailto:flow.support@protonmail.com"
-            id="contact-btn"
-            class="btn-main"
-            aria-label="contact"
-          >
-            <i class="fa fa-envelope"></i>
-            Contact Us
-          </a>
+  <footer class="has-background-light">
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column has-text-centered">
+            <logo id="foot_logo" />
+          </div>
+          <div class="column has-text-centered">
+            <h3 class="title is-4">Find Us At</h3>
+            <ul id="social">
+              <li>
+                <a
+                  href="https://www.twitter.com/Flow_ArtInt"
+                  aria-label="twitter"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  <i class="fab fa-twitter"></i>
+                  Twitter
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com/company/flow-api"
+                  aria-label="linkedin"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  <i class="fab fa-linkedin"></i>
+                  LinkedIn
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.instagram.com/flow_artint/"
+                  aria-label="instagram"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  <i class="fab fa-instagram"></i>
+                  Instagram
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.facebook.com/flowml"
+                  aria-label="facebook"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  <i class="fab fa-facebook"></i>
+                  Facebook
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="column has-text-centered">
+            <h3 class="title is-4">Contact Info</h3>
+            <a
+              href="mailto:flow.support@protonmail.com"
+              id="contact-btn"
+              class="btn-main"
+              aria-label="contact"
+            >
+              <i class="fa fa-envelope"></i>
+              Contact Us
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   </footer>
 </template>
 <script>
