9e341cb2f5b8bd05054fc6313975789a1cd19336
Karamveer Singh <kmvsingh007@gmail.com>
2019-11-05 13:47:37 +0530

    prettier and darkMode init

--- layouts/public.vue
+++ layouts/public.vue
@@ -3,6 +3,9 @@
     <nav-bar />
     <nuxt />
     <!-- <foot /> -->
+    <div id="dark-mode" class="has-background-black" @click="toggleDarkMode">
+      <i class="fa fa-moon has-text-light"></i>
+    </div>
     <copyright-strip />
   </div>
 </template>
@@ -16,6 +19,37 @@ export default {
     NavBar,
     // Foot,
     CopyrightStrip
+  },
+  data() {
+    return {
+      darkMode: false
+    }
+  },
+  methods: {
+    toggleBodyClass(addRemoveClass, className) {
+      const el = document.body
+      const vm = this
+      if (addRemoveClass === 'addClass') {
+        el.classList.add(className)
+        vm.darkMode = true
+      } else {
+        el.classList.remove(className)
+        vm.darkMode = false
+      }
+    },
+    toggleDarkMode() {
+      if (!this.darkMode) this.toggleBodyClass('addClass', 'dark-mode')
+      else this.toggleBodyClass('removeClass', 'dark-mode')
+    }
   }
 }
 </script>
+<style scoped>
+#dark-mode {
+  position: fixed;
+  border-radius: 50%;
+  right: 20px;
+  bottom: 20px;
+  padding: 15px 20px;
+}
+</style>
