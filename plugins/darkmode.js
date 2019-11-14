d6d7aee7146eb6b294bacfc8d1dc20b444cbf3d7
Sanjampreet Singh <sanjam531@gmail.com>
2019-11-15 01:37:37 +0530

    darkmode as per device

--- plugins/darkmode.js
+++ plugins/darkmode.js
@@ -1,8 +1,17 @@
+/* eslint-disable */
 export default function() {
-  const el = document.body
-  if (window.localStorage.getItem('auth.dark_mode') === '1') {
-    el.classList.add('dark-mode')
-  } else {
-    el.classList.remove('dark-mode')
-  }
+    const el = document.body
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        if (window.localStorage.getItem('auth.dark_mode') === '1') {
+            el.classList.add('dark-mode')
+        } else if (window.localStorage.getItem('auth.dark_mode') === '0') {
+            el.classList.remove('dark-mode')
+        } else {
+            el.classList.add('dark-mode')
+        }
+    } else {
+        el.classList.remove('dark-mode')
+    }
 }
+
+// This.darkmode pending
