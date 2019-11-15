d8224dd145643aabf671fa624d8932fa37c8dd65
Sanjampreet Singh <sanjam531@gmail.com>
2019-11-15 13:51:52 +0530

    error debugged

--- plugins/darkmode.js
+++ plugins/darkmode.js
@@ -2,16 +2,8 @@
 export default function() {
     const el = document.body
 
-    //  If browser does not support the `prefers-color-scheme` media query.
-    if (window.matchMedia('(prefers-color-scheme)').media === 'not all') {
-        if (window.localStorage.getItem('auth.dark_mode') === '1') {
-            el.classList.add('dark-mode')
-        } else {
-            el.classList.remove('dark-mode')
-        }
-    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches)
     //If browser support the `prefers-color-scheme` media query.
-    {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
         if (window.localStorage.getItem('auth.dark_mode') === '1') {
             el.classList.add('dark-mode')
         } else if (window.localStorage.getItem('auth.dark_mode') === '0') {
@@ -19,6 +11,12 @@ export default function() {
         } else {
             el.classList.add('dark-mode')
         }
+    } else {
+        el.classList.remove('dark-mode')
+    }
+
+    if (window.localStorage.getItem('auth.dark_mode') === '1') {
+        el.classList.add('dark-mode')
     } else {
         el.classList.remove('dark-mode')
     }
